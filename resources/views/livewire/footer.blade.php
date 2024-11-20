ec8ff7a7ac4fba3e47aa5ca6460662f080ab451c
juan ramon <juanramoncornejo@gmail.com>
2024-11-20 20:45:21 -0300

    Se añade footer

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <footer class="bg-gray-800 text-white py-4 px-4">
+
+        <div class="container mx-auto text-center mt-2">
+            © 2024 Asociacion de Futbol Catemu. Todos los derechos reservados.
+        </div>
+
+        <div class="container mx-auto text-center">
+            Sitio Desarrollo por JRDEVELOPER
+        </div>
+    </footer>
 </div>
